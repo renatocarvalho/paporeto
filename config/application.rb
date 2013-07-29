07997a25d2c82e52333a873e7a518dc903c437ca
Renato Carvalho <renatolz@gmail.com>
2013-07-29 20:14:29 -0300

    Add config files
    Signed-off-by: Renato Carvalho <renatolz@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -13,16 +13,14 @@ Bundler.require(:default, Rails.env)
 
 module Paporeto
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.i18n.default_locale = 'pt-BR'
     config.time_zone = 'Brasilia'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.generators do |g|
+        g.stylesheet_engine = :sass
+        g.assets            false
+        g.helper            false
+        g.view_specs        false
+        g.helper_specs      false
+    end
   end
 end
