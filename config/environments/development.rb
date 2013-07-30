4cbe71211499780df284de02d97e79bd5bff1cee
Renato Carvalho <renatolz@gmail.com>
2013-07-29 22:40:52 -0300

    Add parametro no development.rb para mostrar erro de segurança do rails que fica oculto
    Signed-off-by: Renato Carvalho <renatolz@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,9 @@ Paporeto::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # By default parameter keys that are not explicitly permitted will be logged
+  # in the development and test environment. In other environments these parameters
+  # will simply be filtered out and ignored.
+  config.action_controller.action_on_unpermitted_parameters = :raise
 end
